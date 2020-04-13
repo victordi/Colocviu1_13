ebd33566b05f243526115d9aa92061f13aec0426
victordi <diaconuvictor11@yahoo.com>
2020-04-13 10:42:13 +0300

    A

---
+++ Colocviu1_13/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Colocviu1_13MainActivity.java
@@ -0,0 +1,21 @@
+package ro.pub.cs.systems.eim.Colocviu1_13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Colocviu1_13MainActivity extends AppCompatActivity {
+
+    EditText text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textView8);
+    }
+
+}
